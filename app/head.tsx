630ebf5ef51e0452f1a3ab5f775adaff6e1e3194
Lisili Richie <lisilirichie@gmail.com>
2026-07-19 10:59:30 +0300

    Update favicon and clean public assets

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,7 @@ export default function Head() {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" />
-      <title>Create Next App</title>
+    <title>tulia satys</title>
     </>
   );
 }
